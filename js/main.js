f5a424ac4a6e94df676f0326abdf9f6d006d80d1
SweetAudio <brad@sweetaudio.co.uk>
2026-05-19 18:16:29 +0100

    set default for yt without thumbnail

--- js/main.js
+++ js/main.js
@@ -2,11 +2,30 @@ const grid = document.getElementById("projects-grid");
 
 function getYoutubeThumbnail(url){
 
-  if(url.includes("youtu.be/")){
+  try{
 
-  const id = url.split("youtu.be/")[1].split("?")[0];
+    if(url.includes("youtu.be/")){
 
-  return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+      const id = url.split("youtu.be/")[1].split("?")[0];
+
+      return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+    }
+
+    if(url.includes("youtube.com/watch?v=")){
+
+      const id = url.split("v=")[1].split("&")[0];
+
+      return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+    }
+
+  }
+
+  catch{
+
+    return "images/bts/default.jpg";
+  }
+
+  return "images/bts/default.jpg";
 }
 
 if(url.includes("youtube.com/watch?v=")){
